0aa86bb9fea6f7ed74b8e90ffd537c14c3202432
Abhishek Parmal <abhishekparmal512@gmail.com>
2025-06-14 19:29:15 +0530

    Initial commit - Apna Video Call frontend and backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,15 +23,12 @@ app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
     app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://imdigitalashish:imdigitalashish@cluster0.cujabk4.mongodb.net/")
+    const connectionDb = await mongoose.connect("mongodb+srv://abhishekparmal:abhishekparmal09@zoomcluster.n7txfrw.mongodb.net/zoom?retryWrites=true&w=majority&appName=ZoomCluster")
 
     console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
     server.listen(app.get("port"), () => {
         console.log("LISTENIN ON PORT 8000")
     });
-
-
-
 }
 
 
